db4cbf051586725951eb64a7d27373cff0480ca7
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-04-03 11:30:31 -0400

    feat: adds production server script

--- src/scripts.py
+++ src/scripts.py
@@ -4,10 +4,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+@click.group()
+def server():
+    pass
+
+
+@server.command()
 def dev():
     from uvicorn import run
 
     run("src.asgi:app", reload=True, reload_dirs="src")
+
+
+@server.command()
+def prod():
+    from subprocess import run
+
+    run("gunicorn -w 4 -k uvicorn.workers.UvicornWorker src.asgi:app".split(" "))
 
 
 @click.group()
