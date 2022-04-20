16d67520099cbeaa4d6f5fbe11e22adabc097744
Demetrius Moreira Panovitch <demetrius.mp789@gmail.com>
2022-04-19 22:45:49 -0300

    refactor: fix dependency error

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,9 +1,11 @@
+from __future__ import annotations
+
 from fastapi import Depends
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from src.core.security import decode_jwt, oauth2_scheme
 from src.database import engine
-from src.repository.user import get_user_by_email
+from src.models import User
 
 from .http_exceptions import credentials_exception
 
@@ -22,6 +24,6 @@ def get_current_user(
     if token_data is None or token_data.email is None:
         raise credentials_exception
 
-    user = get_user_by_email(db, email=token_data.email)
+    stmt = select(User).where(User.email == token_data.email)
 
-    return user
+    return db.exec(stmt).first()
