5d92d4b3272383e5188cd30159ab96e0a7f63d5b
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-05-03 18:24:59 -0400

    refactor: remove unused model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,8 +1,7 @@
-from datetime import date, datetime
-from typing import List, Optional
+from typing import Optional
 
 from pydantic import EmailStr
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Field, SQLModel
 
 
 # define your database tables (models) here
@@ -13,18 +12,3 @@ class User(SQLModel, table=True):
         nullable=False,
     )
     password: str = Field(nullable=False)
-
-    invoices: List["Invoice"] = Relationship(back_populates="user")
-
-
-class Invoice(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, nullable=False, primary_key=True)
-    title: str = Field(nullable=False)
-    amount: float = Field(nullable=False)
-    description: Optional[str] = Field(nullable=True, default=None)
-    due_date: Optional[date] = Field(nullable=True, default=None)
-
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-
-    user_id: int = Field(nullable=False, foreign_key="user.id")
-    user: User = Relationship(back_populates="invoices")
--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,3 +1,2 @@
 # import your input/output schemas here
-from .invoice import CreateInvoice, ReadInvoice
 from .user import CreateUser, CurrentUser, UpdateUser
